398e741a4f0fa0c64d7428b744d80184cc5d243c
aidan daffa <muhammad.aidan@ui.ac.id>
2021-11-16 16:07:56 +0700

    test

---
+++ lib/pages/advertise_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Advertise extends StatefulWidget {
+  @override
+  _AdvertiseState createState() => _AdvertiseState();
+}
+
+class _AdvertiseState extends State<Advertise> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Advertise testing'),
+    );
+  }
+}
